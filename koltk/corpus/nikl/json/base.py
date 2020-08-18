f08675ef00911dde1e20ae89174439aea80de957
youhyunjo <1140127+youhyunjo@users.noreply.github.com>
2020-08-18 17:17:03 +0900

    (new) koltk/corpus/nikl/json

---
+++ koltk/corpus/nikl/json/base.py
@@ -0,0 +1,37 @@
+"""
+from koltk.corpus.nikl.json import 
+"""
+
+import json
+
+class NIKLJSON(dict):
+    """
+    NIKLJSON object.
+    """ 
+
+    def __init__(self, iterable=(), **extra):
+        """
+        :return: a NIKLJSON object
+        """
+        super().__init__(iterable)
+        self.update(extra)
+
+    def __repr(self):
+        return json.dumps(self, ensure_ascii=False)
+
+    def __str__(self):
+        return json.dumps(self, ensure_ascii=False, indent=2)
+    
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
+
+    
